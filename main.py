04a714550a22a191405215dbc447a8490c880022
Bhavya Muni <bhavyamuni@outlook.com>
2022-12-21 16:25:07 -0500

    can now create events

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ import aiohttp
 
 # from typing import *
 
-SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"]
+SCOPES = ["https://www.googleapis.com/auth/calendar"]
 
 load_dotenv()
 
@@ -21,9 +21,7 @@ async def get_shifts():
         url = "https://myschedule.metro.ca/api/"
         await session.get(f"{url}Login/{os.getenv('METRO-LOGIN')}")
         async with session.get(f"{url}Employee/") as data:
-            json_data = await data.json()
-            shifts = json_data["WorkTime"]
-        return shifts
+            return await data.json()["WorkTime"]
 
 
 def auth():
@@ -47,10 +45,43 @@ def auth():
         print("An error occurred: %s" % error)
 
 
+def get_event(start_time, end_time):
+    return {
+        "summary": "Starbucks",
+        "location": "444 Yonge St, Toronto, ON M5B 2H4",
+        "start": {
+            "dateTime": start_time,
+            "timeZone": "America/New_York",
+        },
+        "end": {
+            "dateTime": end_time,
+            "timeZone": "America/New_York",
+        },
+        "reminders": {"useDefault": True},
+    }
+
+
+def get_times(time, date):
+    time_s = time.split("-")[0] + ":00"
+    time_e = time.split("-")[1].split(" ")[0] + ":00"
+    start_time = date[: -len(time_s)] + time_s
+    end_time = date[: -len(time_e)] + time_e
+    return (start_time, end_time)
+
+
 async def main():
     shifts = await get_shifts()
-    for i in shifts:
-        print(i)
+    with auth() as service:
+        for i in [shifts[0]]:
+            if i["DailySeconds"] > 0:
+                s, e = get_times(i["DailyShift"][0], i["StartDate"])
+                event = get_event(s, e)
+                event = (
+                    service.events()
+                    .insert(calendarId=os.getenv("CALENDAR-ID"), body=event)
+                    .execute()
+                )
+                print("Event created: %s" % (event.get("htmlLink")))
 
 
 if __name__ == "__main__":
